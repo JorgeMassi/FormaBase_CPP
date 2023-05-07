727b7c308280cfde27d0e0dce4ad835f548f3ae6
Jorge Massi <104774521+JorgeMassi@users.noreply.github.com>
2023-05-07 12:19:02 +0100

    algorithms in C++

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string fristLine, secondLine;
+
+    cout <<"Frist string"<< endl;
+    getline(cin, fristLine);
+
+    vector<char>vectorFristString;
+    for(char letter : fristLine)
+    {
+        if (letter != 32)
+            {
+                vectorFristString.push_back(letter);
+            }
+    }
+    sort(vectorFristString.begin(), vectorFristString.end());
+
+    cout<< "Second String"<< endl;
+    getline(cin, secondLine);
+
+    vector<char> vectorSecondString;
+    for (char secondLetter : secondLine)
+    {
+        if (secondLetter != 32)
+        {
+            vectorSecondString.push_back(secondLetter);
+        }
+    }
+    sort(vectorSecondString.begin(), vectorSecondString.end());
+
+    bool anagrams = equal(vectorFristString.begin(), vectorFristString.end(), vectorSecondString.begin());
+
+    if (anagrams == false )
+    {
+        cout<<"NOT ANAGRAMS"<< endl;
+    }
+    else
+    {
+        cout<<"THEY ARE ANAGRAMS"<< endl;
+    }
+}
